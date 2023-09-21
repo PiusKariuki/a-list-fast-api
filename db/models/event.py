b033cedb04b369277a03dde7adb038db9505b90e
PiusKariuki <piuskariuki19@gmail.com>
2023-09-21 08:30:33 +0300

    second commit

---
+++ db/models/event.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from datetime import datetime
+
+
+class Event(Base):
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    start_time = Column(DateTime, nullable=False)
+    end_time = Column(DateTime, nullable=False)
+    created_at = Column(DateTime, default=datetime.now)
---
+++ main.py
@@ -0,0 +1,5 @@
+from fastapi import FastAPI
+
+
+def start_application():
+    app = FastAPI(title)
